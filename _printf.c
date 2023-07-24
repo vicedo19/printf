5124963a658c6bbc473e73a256621a5b7e45bca7
FearGod Okeke <okekefeargod@gmail.com>
2023-07-24 05:56:02 -0700

    add customised printf function that handles c, s, and % formats

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-int str_length(*str)
+int str_length(char *str)
 {
 	int i = 0;
 
@@ -13,6 +13,7 @@ int str_length(*str)
 	}
 	return (i);
 }
+
 /**
  * _printf - a function that produces output according to a format.
  * @format: a character string.
@@ -21,5 +22,49 @@ int str_length(*str)
  */
 int _printf(const char *format, ...)
 {
-	char
+	char *separator = "", *str;
+	int i = 0, ch;
+	va_list print;
+
+	va_start(print, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			if (format[i] == '%')
+			{
+				i++;
+				switch(format[i])
+				{
+					case 'c':
+						ch = va_arg(print, int);
+						write(1, separator, 1);
+						write(1, &ch, 1);
+						break;
+
+					case 's':
+						str = va_arg(print, char*); 
+						write(1, separator, 1);
+						write(1, str, str_length(str));
+						break;
+
+					case '%':
+						write(1, separator, 1);
+						write(1, "%", 1);
+						break;
+
+					default:
+						break;
+				}
+				separator = ", ";
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+
+	va_end(print);
+
+	return (i);
 }
