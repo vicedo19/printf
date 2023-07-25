8d0f42aab42eb9f01e61c88e8f61cb3fd7a4cc95
vicedo19 <victorugochukwuka@gmail.com>
2023-07-25 15:57:55 -0700

    modified main header file

--- main.h
+++ main.h
@@ -6,5 +6,7 @@ void print_string(char *str);
 int _putchar(char c);
 void print_char(int ch);
 int _printf(const char *format, ...);
-
+void _flagSpecifier(const char *format, ...);
+void _lengthSpecifier(const char *format, ...);
+void _fieldWidth(const char *format, ...);
 #endif
