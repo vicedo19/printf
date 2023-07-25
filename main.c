6e378b46135828874a689628be8f3c2aa4ebb5d1
FearGod Okeke <okekefeargod@gmail.com>
2023-07-25 08:03:16 -0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "updated printf file"""""""""""""
    This reverts commit 97d63b1960e62dc09f1bb5eb0cf7094c3123fe76.

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int len, len2;
+
+	len = _printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	len2 = printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("different length.\n");
+		fflush(stdout);
+		return (1);
+	}
+	return (0);
+}
